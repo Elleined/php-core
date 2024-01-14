b53fcc9013d71aa903e75396164e0bfb6f45435a
Elleined <demadegu@gmail.com>
2024-01-14 18:11:40 +0800

    added passwordhasing algorithm

---
+++ Hashing/PasswordHashing.php
@@ -0,0 +1,13 @@
+<?
+// Typically the user inputted password in login form
+$password = "myPassword";
+
+// Typically the password hash saved in database
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+echo $hashedPassword . "<br>";
+
+// Extra Step: to get the password hash when user is trying to log in get his password via his email then proceed to next line
+
+// You will compare the user inputted password and his password hash saved in database
+echo password_verify($password, $hashedPassword) ? "Password match" : "Password not match";
+?>
