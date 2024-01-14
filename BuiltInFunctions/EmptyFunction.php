419a06a5939987e0e9221b659021609a99ef5f77
Elleined <demadegu@gmail.com>
2024-01-14 17:19:01 +0800

    added string functions

---
+++ BuiltInFunctions/EmptyFunction.php
@@ -0,0 +1,5 @@
+<?
+
+// isset() function returns true if supplied variable is declared and not null.
+// empty() function returns true if supplied variable is not declared, null, false, and "".
+?>
