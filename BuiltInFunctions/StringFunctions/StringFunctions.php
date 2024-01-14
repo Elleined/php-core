0a2032ffe8d8a0c0a62fa2f839350ad76c9222fa
Elleined <demadegu@gmail.com>
2024-01-14 17:52:01 +0800

    added require in stringfunction.php to call other string functions

---
+++ BuiltInFunctions/StringFunctions/StringFunctions.php
@@ -0,0 +1,21 @@
+<a href="https://www.php.net/manual/en/ref.strings.php">String Functions PHP Documentation</a>
+<?
+require "./Encryption/Crypt.php";
+require "./Encryption/MD5.php";
+require "./Encryption/Sha1.php";
+require "./Contains.php";
+require "./EndsWith.php";
+require "./Implode.php";
+require "./Lower.php";
+require "./MoneyFormat.php";
+require "./RemoveWhiteSpaces.php";
+require "./ReplaceAllOccurances.php";
+require "./Reverse.php";
+require "./Shuffle.php";
+require "./StartsWith.php";
+require "./StringToArray.php";
+require "./Upper.php";
+require "./UpperAllFirstLetter.php";
+require "./UpperFirstLetter.php";
+require "./WordCount.php";
+?>
