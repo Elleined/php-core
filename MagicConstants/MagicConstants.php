0d9fe0395f02eefc86176372144e79dc07920f5d
Elleined <demadegu@gmail.com>
2024-01-16 17:20:34 +0800

    added magic constants .php files

---
+++ MagicConstants/MagicConstants.php
@@ -0,0 +1,12 @@
+<?
+echo "=================<br>";
+echo "MAGIC CONSTANTS <br>";
+echo "=================<br> <hr>";
+
+require "./Class.php";
+require "./Directory.php";
+require "./Field.php";
+require "./File.php";
+require "./Function.php";
+require "./Line.php";
+require "./Method.php";
