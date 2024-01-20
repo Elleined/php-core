b27a5e671e69c53f91b311e80019046c021da0de
Elleined <demadegu@gmail.com>
2024-01-20 23:46:41 +0800

    added files package

--- SuperGlobals/Files/UploadAttachment.php
+++ SuperGlobals/Files/UploadAttachment.php
@@ -1,19 +1,18 @@
 <?php
 declare(strict_types = 1);
-
 $attachment = $_FILES["attachment"];
-if(isAttachmentValid($attachment)) {
-    $attachmentPath = $attachment["tmp_name"];
-    $attachmentName = $attachment["name"];
-    $attachmentSize = $attachment["size"];
-
-    echo "Attachment path: $attachmentPath<br>";
-    echo "Attachment name: $attachmentName<br>";
-    echo "Attachment size: $attachmentSize<br>";
+if(!isAttachmentValid($attachment)) {
+    echo "No File Uploaded!";
     return;
 }
-echo "No File Uploaded!";
 
+$attachmentPath = $attachment["tmp_name"];
+$attachmentName = $attachment["name"];
+$attachmentSize = $attachment["size"];
+
+echo "Attachment path: $attachmentPath<br>";
+echo "Attachment name: $attachmentName<br>";
+echo "Attachment size: $attachmentSize<br>";
 
 function isAttachmentValid(array $attachment): bool {
     return isset($attachment) && $attachment['error'] === UPLOAD_ERR_OK;
